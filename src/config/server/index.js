9e9392a2e44f1ad9ebe50eb12a9ff1edb89ec5bd
matheus.moreira <matheus.moreira@ext.vallourec.com>
2023-11-08 20:21:50 -0300

    feat(info):adds info of all available routes

--- src/config/server/index.js
+++ src/config/server/index.js
@@ -1,27 +1,29 @@
 import express from 'express';
 import cors from 'cors';
-import router from "../../routes/userRoutes.js";
-import dataSource from "../database/index.js";
-import passport from "passport";
-import session from "express-session";
+import router from '../../routes/userRoutes.js';
+import dataSource from '../database/index.js';
+import passport from 'passport';
+import session from 'express-session';
 
 const app = express();
 
 try {
-    await dataSource.initialize();
-    console.log("Conectado ao banco");
+   await dataSource.initialize();
+   console.log('Conectado ao banco');
 } catch (e) {
-    console.error(e);
+   console.error(e);
 }
 
 app.use(cors());
 
 // Add session handling before initializing passport
-app.use(session({
-    secret: process.env.SESSION_SECRET || 'default_secret', // Always use environment variables in production
-    resave: false,
-    saveUninitialized: false
-}));
+app.use(
+   session({
+      secret: process.env.SESSION_SECRET || 'default_secret', // Always use environment variables in production
+      resave: false,
+      saveUninitialized: false,
+   }),
+);
 
 // Initialize Passport
 app.use(passport.initialize());
@@ -31,18 +33,19 @@ app.use(express.json());
 app.use(router);
 
 app.use('*', (_req, res, next) => {
-    const err = new Error('Not Found');
-    res.status(404).send({ message: err.message });
-    next(err);
+   const err = new Error('Not Found');
+   res.status(404).send({ message: err.message });
+   next(err);
 });
 
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 app.use((err, res, _next) => {
-    console.error(err.stack);
-    res.status(500).send({ message: 'Internal server error' });
+   console.error(err.stack);
+   res.status(500).send({ message: 'Internal server error' });
 });
 
 app.set('port', process.env.PORT || 3000);
-console.log("futuras rotas");
+console.log('Rotas:');
+console.log(router.stack.map((item) => item.route.path));
 
 export default app;
